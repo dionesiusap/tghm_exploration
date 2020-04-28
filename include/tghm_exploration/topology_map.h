d0199f9cf6fdb6d5c6b2fb6b58e4ba854dfd4966
Dionesius Agung AP <dionesiusap@gmail.com>
2020-04-29 00:22:22 +0700

    finish tghm implementation

--- include/tghm_exploration/topology_map.h
+++ include/tghm_exploration/topology_map.h
@@ -3,54 +3,76 @@
 
 #include <costmap_2d/costmap_2d.h>
 
-namespace topology_map
+namespace tghm
 {
 /**
  * @brief Represents a node in topology map
  *
  */
 struct TopologyNode {
-  std::uint32_t size;
-  double cost;
+  std::uint32_t unknown_cells;
+  double score;
   bool visited;
   geometry_msgs::Point centroid;
-  std::vector<TopologyNode*> parents;
-  std::vector<TopologyNode*> children;
+  std::vector<TopologyNode*> neighbors;
+  // search utilities
+  bool temp_v;
+  unsigned int level;
 };
 
 /**
- * @brief Thread-safe implementation of a node-generation task for
+ * @brief Thread-safe implementation of a goal-generation task for
  * TGHM algorithm for an input costmap.
  */
-class TopologyMap {
+class TGHM {
 public:
-  TopologyMap(){}
+  TGHM(){}
 
-  TopologyMap(costmap_2d::Costmap2D* costmap,
+  TGHM(costmap_2d::Costmap2D* costmap,
               double lambda,
               geometry_msgs::Point pose);
 
-  TopologyNode getNextGoal(geometry_msgs::Point pose);
-  
-  void updateTopologyMap(geometry_msgs::Point pose);
+  TopologyNode getNextGoal();
 
+  std::vector<TopologyNode> getMapVector();
+  
 protected:
-  void generateCandidateNodes();
+  void generateNewCandidateNodes(unsigned int pos_x_map,
+                                 unsigned int pos_y_map);
+
   TopologyNode createNode(unsigned int initial_cell,
                           std::vector<bool>& node_flag);
+
+  void updateCandidateNodes();
+  
   bool isNewNodeCell(unsigned int idx,
                      const std::vector<bool>& node_flag);
-  double calculateNodeCost();
+  
+  bool isCellObservableFrom(unsigned int idx,
+                        unsigned int reference);
+  
+  void calculateNodeCost(TopologyNode* node);
+  
+  unsigned int getTopologicalDistance(TopologyNode* candidate_node,
+                                      TopologyNode* map_node);
+  
+  void resetNodeSearch();
 
 private:
   costmap_2d::Costmap2D* costmap_;
   unsigned char grid_map_;
   unsigned int size_x_, size_y_;
 
-  TopologyNode topology_map_;
-  TopologyNode current_node_;
+  std::vector<TopologyNode> map_nodes_;
   std::vector<TopologyNode> candidate_nodes_;
+  TopologyNode* current_node_;
+  TopologyNode* root_node_;
+  
+  // exploration parameters
+  double min_unknown_size_;
   double lambda_;
+  double sensor_max_range_;
+  unsigned int unknown_threshold_;
 };
 }
 #endif
--- src/topology_map.cpp
+++ src/topology_map.cpp
@@ -1,5 +1,5 @@
-#include <explore/topology_map.h>
-#include <explore/costmap_tools.h>
+#include <tghm_exploration/topology_map.h>
+#include <tghm_exploration/costmap_tools.h>
 
 #include <mutex>
 
@@ -7,48 +7,67 @@
 #include <costmap_2d/costmap_2d.h>
 #include <geometry_msgs/Point.h>
 
-namespace topology_map
+namespace tghm
 {
   using costmap_2d::LETHAL_OBSTACLE;
   using costmap_2d::NO_INFORMATION;
   using costmap_2d::FREE_SPACE;
   
-  TopologyMap::TopologyMap(costmap_2d::Costmap2D* costmap,
-                           double lambda,
-                           geometry_msgs::Point pose)
+  TGHM::TGHM(costmap_2d::Costmap2D* costmap,
+             double lambda,
+             geometry_msgs::Point pose)
     : costmap_(costmap)
     , lambda_(lambda)
   {
     TopologyNode root;
     root.centroid = pose;
-    root.cost = 0;
+    root.score = 0;
     root.visited = true;
-    current_node_ = root;
+    map_nodes_.push_back(root);
+    root_node_ = &map_nodes_[0];
+    current_node_ = &map_nodes_[0];
   }
 
-  TopologyNode TopologyMap::getNextGoal(geometry_msgs::Point pose)
+  TopologyNode TGHM::getNextGoal()
   {
     // sanity check that robot is inside costmap bounds before searching
     unsigned int pos_x_map, pos_y_map;
-    if (!costmap_->worldToMap(position.x, position.y, pos_x_map, pos_y_map)) {
-      ROS_ERROR("Robot out of costmap bounds, cannot search for frontiers");
-      return frontier_list;
+    if (!costmap_->worldToMap(current_node_->centroid.x, current_node->centroid.y,
+                              pos_x_map, pos_y_map))
+    {
+      ROS_ERROR("getNextGoal: Robot out of costmap bounds, cannot search for candidate nodes");
+      return current_node_;
     }
-
-    generateCandidateNodes(pos_x_map, pos_y_map);
-  }
-
-  void TopologyMap::generateCandidateNodes(unsigned int pos_x_map,
-                                           unsigned int pos_y_map)
-  {
     // lock the costmap during the generation of nodes
     std::lock_guard<costmap_2d::Costmap2D::mutex_t> lock(*(costmap_->getMutex()));
-    
+
+    generateNewCandidateNodes(pos_x_map, pos_y_map);
+    updateCandidateNodes();
+
+    TopologyNode* best_node = &candidate_nodes_[0];
+    for (std::vector<TopologyNode>::iterator it = candidate_nodes_.begin()
+         it != candidate_nodes_.end(); ++it)
+    {
+      if (*it.score > best_node->score) {
+        best_node = it;
+      }
+    }
+
+    TopologyNode new_map_node = *best_node;
+    map_node_.push_back(new_map_node);
+
+    current_node_ = best_node;
+    return best_node;
+  }
+
+  void TGHM::generateNewCandidateNodes(unsigned int pos_x_map,
+                                       unsigned int pos_y_map)
+  {
     grid_map_ = costmap_->getCharMap();
     size_x_ = costmap_->getSizeInCellsX();
     size_y_ = costmap_->getSizeInCellsY();
 
-    // initialize flag arrays to keep track of visited and frontier cells
+    // initialize flag arrays to keep track of visited and node cells
     std::vector<bool> node_flag(size_x_ * size_y_, false);
     std::vector<bool> visited_flag(size_x_ * size_y_, false);
 
@@ -57,10 +76,13 @@ namespace topology_map
 
     // find closest clear cell to start search
     unsigned int clear, pos = costmap_->getIndex(pos_x_map, pos_y_map);
+    unsigned int reference;
     if (nearestCell(clear, pos, FREE_SPACE, *costmap_)) {
       bfs.push(clear);
+      reference = clear;
     } else {
       bfs.push(pos);
+      reference = pos;
       ROS_WARN("Could not find nearby clear cell to start search");
     }
     visited_flag[bfs.front()] = true;
@@ -70,32 +92,35 @@ namespace topology_map
       bfs.pop();
       // iterate over 4-connected neighbourhood
       for (unsigned nbr : nhood4(idx, *costmap_)) {
-        // add to queue all free, unvisited cells, use descending search in case
-        // initialized on non-free cell
-        if (grid_map_[nbr] <= grid_map_[idx] && !visited_flag[nbr]) {
-          visited_flag[nbr] = true;
-          bfs.push(nbr);
-          // check if cell is new frontier cell (unvisited, NO_INFORMATION, free
-          // neighbour)
-        } else if (isNewNodeCell(nbr, node_flag)) {
-          node_flag[nbr] = true;
-          TopologyNode new_node = createNode(nbr, pos, target, node_flag);
-          // TODO: filter
-          candidate_nodes_.push_back(new_node);
+        if isCellObservableFrom(nbr, reference) {
+          // add to queue all free, unvisited cells, use descending search in case
+          // initialized on non-free cell
+          if (grid_map_[nbr] <= grid_map_[idx] && !visited_flag[nbr]) {
+            visited_flag[nbr] = true;
+            bfs.push(nbr);
+            // check if cell is new frontier cell (unvisited, NO_INFORMATION, free
+            // neighbour)
+          } else if (isNewNodeCell(nbr, node_flag)) {
+            node_flag[nbr] = true;
+            TopologyNode new_node = createNode(nbr, pos, target, node_flag);
+            candidate_nodes_.push_back(new_node);
+          }
         }
       }
     }
   }
 
-  TopologyNode TopologyMap::createNode(unsigned int initial_cell,
-                                       std::vector<bool>& node_flag)
+  TopologyNode TGHM::createNode(unsigned int initial_cell,
+                                std::vector<bool>& node_flag)
   {
     // initialize node structure
     TopologyNode output;
     output.centroid.x = 0;
     output.centroid.y = 0;
-    output.size = 1;
-    output.parents.push_back(&current_node_);
+    output.score = 0;
+    output.unknown_cells = 0;
+    output.neighbors.push_back(current_node_);
+    std::uint32_t temp_size = 1;
 
     unsigned int ix, iy;
     double initial_x, initial_y;
@@ -114,7 +139,7 @@ namespace topology_map
       for (unsigned int nbr : nhood8(idx, *costmap_)) {
         // check if neighbour is a potential frontier cell
         if (isNewNodeCell(nbr, node_flag)) {
-          // mark cell as frontier
+          // mark cell as unknown cell in node
           node_flag[nbr] = true;
           unsigned int mx, my;
           double wx, wy;
@@ -122,7 +147,7 @@ namespace topology_map
           costmap_->mapToWorld(mx, my, wx, wy);
 
           // update node size
-          output.size++;
+          temp_size++;
 
           // update centroid of node
           output.centroid.x += wx;
@@ -134,14 +159,29 @@ namespace topology_map
       }
     }
 
-    output.centroid.x /= output.size;
-    output.centroid.y /= output.size;
+    output.centroid.x /= temp_size;
+    output.centroid.y /= temp_size;
 
-    return output
+    return output;
   }
 
-  bool TopologyMap::isNewNodeCell(unsigned int idx,
-                                  const std::vector<bool>& node_flag)
+  void TGHM::updateCandidateNodes()
+  {
+    std::vector<unsigned int> idxs;
+    for (int i = 0; i < candidate_nodes_.size(); i++) {
+      if (candidate_nodes_.unknown_cells * costmap_->getResolution() <
+          min_unknown_size_) {
+        idx.push_back(i);
+      }
+    }
+    
+    for (i : idxs) {
+      candidate_nodes_.erase(candidate_nodes_.begin()+i);
+    }
+  }
+
+  bool TGHM::isNewNodeCell(unsigned int idx,
+                           const std::vector<bool>& node_flag)
   {
     // check that cell is unknown and not already marked as node
     if (grid_map_[idx] != NO_INFORMATION || node_flag[idx]) {
@@ -158,4 +198,102 @@ namespace topology_map
 
     return false;
   }
+
+  bool TGHM::isCellObservableFrom(unsigned int idx,
+                                  unsigned int reference)
+  {
+    unsigned int idx_x_map, idx_y_map;
+    unsigned int ref_x_map, ref_y_map;
+    double idx_x_world, idx_y_world;
+    double ref_x_world, ref_y_world;
+    
+    costmap_->indexToCells(idx, idx_x_map, idx_y_map);
+    costmap_->indexToCells(reference, ref_x_map, ref_y_map);
+
+    costmap_->mapToWorld(idx_x_map, idx_y_map, idx_x_world, idx_y_world);
+    costmap_->mapToWorld(ref_x_map, ref_y_map, ref_x_world, ref_y_world);
+
+    double distance = sqrt(pow((double(ref_x_map) - double(idx_x_map)), 2.0) +
+                           pow((double(ref_y_map) - double(idx_y_map)), 2.0));
+
+    if (distance <= sensor_max_range_) {
+      for (unsigned int nbr : nhood4(idx, *costmap_)) {
+        if (grid_map_[nbr] == FREE_SPACE || grid_map_[nbr] == NO_INFORMATION)
+          return true;
+      }
+
+    }
+    
+    return false;
+  }
+
+  void TGHM::calculateNodeCost(TopologyNode* node)
+  {
+    unsigned int node_idx = costmap_->getIndex(node->centroid.x, node->centroid.y);
+
+    // push node centroid into queue
+    std::queue<unsigned int> bfs;
+    bfs.push(node_idx);
+
+    while (!bfs.empty()) {
+      unsigned int idx = bfs.front();
+      bfs.pop();
+
+      for (unsigned int nbr : nhood8(idx, *costmap_)) {
+        if (isCellObservableFrom(idx, node_idx)) {
+          if (grid_map_[idx] == NO_INFORMATION) {
+            node->unknown_cells++;
+          }
+          bfs.push(nbr);
+        }
+      }
+    }
+
+    node->score = node->unknown_cells * 
+                    exp(-lambda_ * getTopologicalDistance(node, current_node_));
+  }
+
+  unsigned int TGHM::getTopologicalDistance(TopologyNode* candidate_node,
+                                            TopologyNode* map_node)
+  {
+    bool found = false;
+    resetNodeSearch();
+    candidate_node->temp_v = true;
+    
+    std::queue<TopologyNode*> bfs;
+    bfs.push(candidate_node);
+
+    while (!bfs.empty() || !found) {
+      TopologyNode* curr_node = bfs.front();
+      bfs.pop();
+
+      for (std::vector<TopologyNode*>::iterator it = curr_node->neighbors.begin()
+          it != curr_node->neighbors.end(); ++it)
+      {
+        if (!*it->temp_v) {
+          *it->temp_v = true;
+          *it->level = curr_node->level + 1;
+          bfs.push(*it);
+        }
+        if (*it->centroid == map_node->centroid) {
+          found == true;
+          return *it->level;
+        }
+      }
+    }
+
+    ROS_ERROR("getTopologicalDistance: MAP NODE NOT FOUND");
+    return 0;
+  }
+
+  void TGHM::resetNodeSearch()
+  {
+    for (std::vector<TopologyNode>::iterator it = map_nodes_.begin()
+         it != map_nodes_.end(); ++it)
+      {
+        *it->temp_v = false;
+        *it->level = 0;
+      }
+  }
+
 }
